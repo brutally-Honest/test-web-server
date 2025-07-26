6e77baf963cb0c43ec8576330cfbdf290437734f
karthik <karthikhs1402@gmail.com>
2025-07-26 17:28:11 +0530

    feat(Route): add about route with json response

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,15 +1,20 @@
 package routes
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type Response struct {
+	Message string `json:"message"`
+}
 
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", rootHandler)
-	mux.HandleFunc("/about", helpHandler)
+	mux.HandleFunc("/about", aboutHandler)
 	return mux
 }
 
@@ -17,6 +22,10 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Welcome!\nLaugh at my misery")
 }
 
-func helpHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("About :\n1. Space to cool down / rant\n2. No judgement\n")
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	resp := Response{Message: "About :\n1. Space to cool down / rant\n2. No judgement\n"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(resp)
+
 }
