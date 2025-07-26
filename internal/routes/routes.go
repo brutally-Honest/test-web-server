fecdaa30951b8bb696f724c13d6b8d5f75a3ae6a
karthik <karthikhs1402@gmail.com>
2025-07-26 17:15:50 +0530

    feat(Route): add about route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,9 +9,14 @@ func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/about", helpHandler)
 	return mux
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Welcome!\nLaugh at my misery")
+}
+
+func helpHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("About :\n1. Space to cool down / rant\n2. No judgement\n")
 }
